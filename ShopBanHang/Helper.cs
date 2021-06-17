2594fd4de2925c3bbeab4711c592cdd16e2b579d
yuno856014@gmail.com <yuno856014@gmail.com>
2021-06-17 21:34:45 +0700

    up 3

--- ShopBanHang/Helper.cs
+++ ShopBanHang/Helper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.IO;
 using Newtonsoft.Json;
+using System.Linq;
 
 namespace ShopBanHang
 {
@@ -39,7 +40,7 @@ namespace ShopBanHang
                 Console.WriteLine("1.Select the product you want to buy !");
                 Console.WriteLine("2.View cart !");
                 Console.WriteLine("3.Remove product !");
-                Console.WriteLine("0.Invoice Printing  and Exit Menu !");
+                Console.WriteLine("0.Print bill and Exit Menu !");
                 Console.ResetColor();
                 if(!int.TryParse(Console.ReadLine(),out choice))
                 {
@@ -55,6 +56,7 @@ namespace ShopBanHang
                         VeiwProduct();
                         break;
                     case 3:
+                        //Remove();
                         break;
                     case 0:
                         Bill();
@@ -66,7 +68,7 @@ namespace ShopBanHang
         public static void Buyproduct(List<Phone> phones, out long sum)
         {
             var result = Helper<GioHang>.ReadFile("Phone.json");
-            string choice = "";
+            int choice = 0;
             do
             {
                 Console.ForegroundColor = ConsoleColor.Green;
@@ -112,10 +114,14 @@ namespace ShopBanHang
                 }
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("Do you want to continue shopping?");
-                Console.WriteLine("Please press c to continue, press k to exit!");
+                Console.WriteLine("Please press 1 to continue!");
+                Console.WriteLine("Press 2 to exit!");
                 Console.ResetColor();
-                choice = Console.ReadLine();
-            } while (choice != "k");
+                if(!int.TryParse(Console.ReadLine(),out choice))
+                {
+                    choice = 1;
+                }    
+            } while (choice != 2);
             sum = 0;
             foreach(Phone item in phones)
             {
@@ -124,27 +130,38 @@ namespace ShopBanHang
         }
         public static void VeiwProduct()
         {
-            foreach(Phone menu in phoNe)
+            foreach(Phone item in phoNe)
             {
-                Console.WriteLine(menu.ToString());
+                Console.WriteLine(item.ToString());
             }    
         }   
-        public static void Remove()
-        {
-           
-            
-        }
+        //public static void Remove()
+        //{
+        //    var nameproduct = Helper<GioHang>.ReadFile("Phone.json");
+        //    Console.WriteLine("Enter name remove ");
+        //    string name = Console.ReadLine();
+        //    foreach(Phone item in nameproduct.Phone)
+        //    {
+        //        if (item.NameProduct.ToLower() == name.ToLower())
+        //        {
+        //            phoNe.Remove(item);
+        //        }
+        //        else Console.WriteLine("Product is not food");
+        //    }
+        //}
         public static void buyProduct()
         {
-            Helper<Phone>.Buyproduct(phoNe, out long sum);
+            Helper<GioHang>.Buyproduct(phoNe, out long sum);
             using (StreamWriter sw = File.CreateText(Path.Combine(Common.FilePath, billFile)))
             {
                 sw.WriteLine($"Bill : {sum} VND");
             }
+            
         }   
         public static void Bill()
         {
             Helper<GioHang>.WriteFile(billFile, phoNe);
+            
             Console.WriteLine("Thanks !");
         }
     }
